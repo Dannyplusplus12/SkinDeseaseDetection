af3a45149df1fdd41030c690b1c8052677ab3cc4
Dannyplusplus12 <hoanghuynhbahuy@gmail.com>
2024-07-15 12:31:45 +0700

    f

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 
 from inference_sdk import InferenceHTTPClient
-# CLIENT = InferenceHTTPClient(
-#     api_url="https://detect.roboflow.com",
-#     api_key="kTK0gAawSv3eqEDeXfAs"
-# )
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="kTK0gAawSv3eqEDeXfAs"
+)
 
 app = Flask(__name__)
 
@@ -12,13 +12,13 @@ app = Flask(__name__)
 def dash():
     return render_template('app.html')
 
-# @app.route("/api", methods=['GET', 'POST'])
-# def api():
-#     if request.method == 'GET':
-#         return jsonify(1)
-#     if request.method == 'POST':
-#         result = CLIENT.infer(request.form['img'], model_id="yolov8-skin-disease-detection/1")
-#         return result
+@app.route("/api", methods=['GET', 'POST'])
+def api():
+    if request.method == 'GET':
+        return jsonify(1)
+    if request.method == 'POST':
+        result = CLIENT.infer(request.form['img'], model_id="yolov8-skin-disease-detection/1")
+        return result
 
 @app.route('/favicon.ico') 
 def favicon(): 
